a544d81863f9db60b5e654c5c045652aa1491753
wuuhii <wuuhii@outlook.com>
2020-02-18 22:33:51 +0800

    add file comment

--- src/MainWindow.cc
+++ src/MainWindow.cc
@@ -1,3 +1,13 @@
+﻿/*
+ * Copyritht 2020 QTCRCCALCULATOR PROJECT <https://github.com/qter188/QtCRCCalculator>
+ * QtCRCCalculator is licensed according to the terms in the file LICENCE in the root of the source code directory.
+ *
+ * The file is encoding with UTF8 (with BOM). Thanks google translation <https://translate.google.cn>.
+ *
+ * Bug Report: qter@188.com
+ * User Exchange QQ Group: 952218522
+ * Qt Technical Exchange QQ Group: 723516989
+ */
 #include "MainWindow.hh"
 #include "ui_MainWindow.h"
 
