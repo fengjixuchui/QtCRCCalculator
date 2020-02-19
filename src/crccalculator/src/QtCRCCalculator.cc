3ee75da9107e8ccbeaeb63fbff65bc27127aa3bb
wuuhii <wuuhii@outlook.com>
2020-02-19 22:44:25 +0800

    register custom type

--- src/crccalculator/src/QtCRCCalculator.cc
+++ src/crccalculator/src/QtCRCCalculator.cc
@@ -8,15 +8,39 @@
  * User Exchange QQ Group: 952218522
  * Qt Technical Exchange QQ Group: 723516989
  */
+#include <QQmlEngine>
 #include "QtCRCCalculator.hh"
 
+QtCRCCalculator *QtCRCCalculator::instancePtr = Q_NULLPTR;
 QtCRCCalculator::QtCRCCalculator(QObject *parent)
     : QObject(parent)
 {
-
+    QQmlEngine::setObjectOwnership(this, QQmlEngine::CppOwnership);
 }
 
 QtCRCCalculator::~QtCRCCalculator()
 {
 
+}
+
+QtCRCCalculator* QtCRCCalculator::instance()
+{
+    if (!instancePtr){
+        new QtCRCCalculator;
+    }
+
+    Q_ASSERT_X(instancePtr, __FUNCTION__, "Can not allocate memory for QtCRCCalculator");
+    return instancePtr;
+}
+
+void QtCRCCalculator::calculatreCRC(QString data, int model, int textModel)
+{
+
+}
+
+void QtCRCCalculator::registerQmlType()
+{
+    qmlRegisterSingletonType<QtCRCCalculator>("CustomControls.QtCalculator", 1, 0, "QtCRCCalculator", [](QQmlEngine *, QJSEngine *)->QObject*{
+        return QtCRCCalculator::instance();
+    });
 }
--- src/crccalculator/src/QtCRCCalculator.hh
+++ src/crccalculator/src/QtCRCCalculator.hh
@@ -12,13 +12,45 @@
 #define QTCRCCALCULATORT_HH
 
 #include <QObject>
+#include "QtCRCInterface.hh"
 
 class QtCRCCalculator : public QObject
 {
     Q_OBJECT
-public:
+private:
     explicit QtCRCCalculator(QObject *parent = Q_NULLPTR);
     ~QtCRCCalculator();
+public:
+    enum TextModel{
+        TextModelFile,
+        TextModelHex,
+        TextModelDec,
+        TextModelOtc,
+        TextModelBin,
+        TextModelUtf8,
+        TextModelLocal
+    };
+    Q_ENUM(TextModel)
+
+    /**
+     * @brief instance 本类是一个单实例类，该函数获取唯一单实例（未被创建则创建）
+     * @return crc计算器实例
+     */
+    static QtCRCCalculator* instance();
+
+    /**
+     * @brief registerQmlType 将该类注册为qml单实例控件，使用时导入即可：import CustomControls.QtCalculator
+     */
+    void registerQmlType();
+
+    /**
+     * @brief calculatreCRC 计算crc值
+     * @param data 待计算数据（文本）,如果需要计算文件的crc，则data是文件名称（绝对路径）
+     * @param model 文本格式，详见TextModel
+     */
+    Q_INVOKABLE void calculatreCRC(QString data, int model, int textModel);
+private:
+    static QtCRCCalculator *instancePtr;
 };
 
 #endif // QTCRCCALCULATOR_HH
