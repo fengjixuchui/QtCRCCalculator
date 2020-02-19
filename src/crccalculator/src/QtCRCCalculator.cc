7a2ae1dbdd318bf282d801197be55abf770bc422
wuuhii <wuuhii@outlook.com>
2020-02-19 21:48:48 +0800

    remove useless code

--- src/crccalculator/src/QtCRCCalculator.cc
+++ src/crccalculator/src/QtCRCCalculator.cc
@@ -8,145 +8,15 @@
  * User Exchange QQ Group: 952218522
  * Qt Technical Exchange QQ Group: 723516989
  */
-#include "CRCCalculatorWidget.hh"
-#include "ui_CRCCalculatorWidget.h"
+#include "QtCRCCalculator.hh"
 
-CRCCalculatorWidget::CRCCalculatorWidget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::CRCCalculatorWidget)
-{
-    ui->setupUi(this);
-
-    inputTextEdit = ui->inputTextEdit;
-    importFilePushButton = ui->importFilePushButton;
-    fileNameLabel = ui->fileNameLabel;
-    fileRadioButton = ui->fileRadioButton;
-    hexRadioButton = ui->hexRadioButton;
-    decRadioButton = ui->decRadioButton;
-    otcRadioButton = ui->otcRadioButton;
-    binRadioButton = ui->binRadioButton;
-    asciiRadioButton = ui->asciiRadioButton;
-    utf8RadioButton = ui->utf8RadioButton;
-    localRadioButton = ui->localRadioButton;
-    progressBar = ui->progressBar;
-
-    modelComboBox = ui->modelComboBox;
-    inputReversalCheckBox = ui->inputReversalCheckBox;
-    outputReversalCheckBox = ui->outputReversalCheckBox;
-    dataWidthComboBox = ui->dataWidthComboBox;
-    polynomialLineEdit = ui->polynomialLineEdit;
-    initValueLineEdit = ui->initValueLineEdit;
-    resultXORValueLineEdit = ui->resultXORValueLineEdit;
-
-    resultHexLineEdit = ui->resultHexLineEdit;
-    resultBinLineEdit = ui->resultBinLineEdit;
-    resultDecLineEdit = ui->resultDecLineEdit;
-    resultOtcLineEdit = ui->resultOtcLineEdit;
-    copyHexPushButton = ui->copyHexPushButton;
-    copyBinPushButton = ui->copyBinPushButton;
-    copyDecPushButton = ui->copyDecPushButton;
-    copyOtcPushButton = ui->copyOtcPushButton;
-
-    visitWebPushButton = ui->visitWebPushButton;
-    calculatePushButton = ui->calculatePushButton;
-}
-
-CRCCalculatorWidget::~CRCCalculatorWidget()
-{
-    delete ui;
-}
-
-void CRCCalculatorWidget::on_importFilePushButton_clicked()
+QtCRCCalculator::QtCRCCalculator(QObject *parent)
+    : QObject(parent)
 {
 
 }
 
-void CRCCalculatorWidget::on_fileRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_hexRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_decRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_otcRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_binRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_asciiRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_utf8RadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_localRadioButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_modelComboBox_currentIndexChanged(const int index)
-{
-    Q_UNUSED(index);
-}
-
-void CRCCalculatorWidget::on_inputReversalCheckBox_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_outputReversalCheckBox_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_dataWidthComboBox_currentIndexChanged(const int index)
-{
-    Q_UNUSED(index);
-}
-
-void CRCCalculatorWidget::on_copyHexPushButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_copyBinPushButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_copyDecPushButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_copyOtcPushButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_visitWebPushButton_clicked()
-{
-
-}
-
-void CRCCalculatorWidget::on_calculatePushButton_clicked()
+QtCRCCalculator::~QtCRCCalculator()
 {
 
 }
--- src/crccalculator/src/QtCRCCalculator.hh
+++ src/crccalculator/src/QtCRCCalculator.hh
@@ -8,96 +8,17 @@
  * User Exchange QQ Group: 952218522
  * Qt Technical Exchange QQ Group: 723516989
  */
-#ifndef CRCCALCULATORWIDGET_HH
-#define CRCCALCULATORWIDGET_HH
+#ifndef QTCRCCALCULATOR_HH
+#define QTCRCCALCULATORT_HH
 
-#include <QLabel>
-#include <QWidget>
-#include <QComboBox>
-#include <QLineEdit>
-#include <QTextEdit>
-#include <QCheckBox>
-#include <QPushButton>
-#include <QRadioButton>
-#include <QProgressBar>
+#include <QObject>
 
-namespace Ui {
-    class CRCCalculatorWidget;
-}
-class CRCCalculatorWidget : public QWidget
+class QtCRCCalculator : public QObject
 {
     Q_OBJECT
 public:
-    explicit CRCCalculatorWidget(QWidget *parent = Q_NULLPTR);
-    ~CRCCalculatorWidget();
-private:
-    Ui::CRCCalculatorWidget *ui;
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief 输入设置
-    QTextEdit *inputTextEdit;
-    QPushButton *importFilePushButton;
-    QLabel *fileNameLabel;
-    QRadioButton *fileRadioButton;
-    QRadioButton *hexRadioButton;
-    QRadioButton *decRadioButton;
-    QRadioButton *otcRadioButton;
-    QRadioButton *binRadioButton;
-    QRadioButton *asciiRadioButton;
-    QRadioButton *utf8RadioButton;
-    QRadioButton *localRadioButton;
-    QProgressBar *progressBar;
-private slots:
-    void on_importFilePushButton_clicked();
-    void on_fileRadioButton_clicked();
-    void on_hexRadioButton_clicked();
-    void on_decRadioButton_clicked();
-    void on_otcRadioButton_clicked();
-    void on_binRadioButton_clicked();
-    void on_asciiRadioButton_clicked();
-    void on_utf8RadioButton_clicked();
-    void on_localRadioButton_clicked();
-private:
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief 参数模型
-    QComboBox *modelComboBox;
-    QCheckBox *inputReversalCheckBox;
-    QCheckBox *outputReversalCheckBox;
-    QComboBox *dataWidthComboBox;
-    QLineEdit *polynomialLineEdit;
-    QLineEdit *initValueLineEdit;
-    QLineEdit *resultXORValueLineEdit;
-private slots:
-    void on_modelComboBox_currentIndexChanged(const int index);
-    void on_inputReversalCheckBox_clicked();
-    void on_outputReversalCheckBox_clicked();
-    void on_dataWidthComboBox_currentIndexChanged(const int index);
-private:
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief 计算结果
-    QLineEdit *resultHexLineEdit;
-    QLineEdit *resultBinLineEdit;
-    QLineEdit *resultDecLineEdit;
-    QLineEdit *resultOtcLineEdit;
-    QPushButton *copyHexPushButton;
-    QPushButton *copyBinPushButton;
-    QPushButton *copyDecPushButton;
-    QPushButton *copyOtcPushButton;
-private slots:
-    void on_copyHexPushButton_clicked();
-    void on_copyBinPushButton_clicked();
-    void on_copyDecPushButton_clicked();
-    void on_copyOtcPushButton_clicked();
-private:
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief 其他操作
-    QPushButton *visitWebPushButton;
-    QPushButton *calculatePushButton;
-private slots:
-    void on_visitWebPushButton_clicked();
-    void on_calculatePushButton_clicked();
-signals:
-
+    explicit QtCRCCalculator(QObject *parent = Q_NULLPTR);
+    ~QtCRCCalculator();
 };
 
-#endif // CRCCALCULATORWIDGET_HH
+#endif // QTCRCCALCULATOR_HH
